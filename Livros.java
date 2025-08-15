0e37226f7b10f985df1ccb47f0a685e374f516d2
Leonardo Gomes Ferreira <ferreiragomesleo26@gmail.com>
2025-08-15 18:13:15 -0300

    Create Livros.java

---
+++ Livros.java
@@ -0,0 +1,11 @@
+public class Livros {
+    String titulo;
+    String autor;
+    String anoPublicacao;
+
+    Livros( String titulo, String autor, String anoPublicacao){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+}
