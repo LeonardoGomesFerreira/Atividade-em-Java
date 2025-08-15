4aa17de525b5d2e17c218f87310c63373d7d7582
Leonardo Gomes Ferreira <ferreiragomesleo26@gmail.com>
2025-08-15 18:14:26 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,11 @@
+public class Cliente {
+    String nome;
+    String cpf;
+    String telefone;
+
+    Cliente(String nome, String cpf, String telefone){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+}
